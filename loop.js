1ce9b05e724531eb276e93485655bf76ead2ecda
Ash1eM <ashley.morris101@yahoo.com>
2023-02-19 18:07:10 -0500

    new

--- loop.js
+++ loop.js
@@ -17,13 +17,6 @@ for(let i = 1000; i >=0; i -=2) {
 
 // favorite shows
 const favShow=["Teen Titans","New Girl","Community","The Amazing World of Gumball","Big Time Rush"]
-const firstShow= favShow[0]
-const secShow= favShow[1]
-const thirdShow= favShow[2]
-const fourthShow= favShow[3]
-const fifthShow= favShow[4]
-console.log("My #1 favorite tv show is", firstShow)
-console.log("My #2 favorite tv show is", secShow)
-console.log("My #3 favorite tv show is", thirdShow)
-console.log("My #4 favorite tv show is", fourthShow)
-console.log("My #5 favorite tv show is", fifthShow)
+for(i = 0; i < favShow.length; i++) {
+    console.log("My #1 favorite show is", favShow[i]);
+}
